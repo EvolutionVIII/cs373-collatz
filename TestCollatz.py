3bfdb505006dd699b07f33b409334c73cdb73f4c
Bao Nguyen <mannywrxnguyen@gmail.com>
2015-06-17 18:10:51 -0500

    working test

--- TestCollatz.py
+++ TestCollatz.py
@@ -15,7 +15,8 @@
 from io       import StringIO
 from unittest import main, TestCase
 
-from Collatz import collatz_read, collatz_singleval, collatz_eval, collatz_print, collatz_solve
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
+#from Collatz import collatz_read, collatz_singleval, collatz_eval, collatz_print, collatz_solve
 
 # -----------
 # TestCollatz
@@ -132,7 +133,7 @@ class TestCollatz (TestCase) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
-        self.assertEqual(w.getvalue(), "1 10 1\n100 200 1\n201 210 1\n900 1000 1\n")
+        self.assertEqual(w.getvalue(), "1 10 20\n100 200 125\n201 210 89\n900 1000 174\n")
 
 # ----
 # main
